6a23909d075e367aacbb3e8583e90e45a817e291
Sunbalcony <160632460@qq.com>
2021-09-03 00:08:34 +0800

    Update 455分发饼干.go

--- fourthWeek/455分发饼干.go
+++ fourthWeek/455分发饼干.go
@@ -17,18 +17,19 @@ func findContentChildren(g []int, s []int) int {
 	sort.Ints(s)
 	gLen := len(g)
 	sLen := len(s)
-	i, j := 0, 0
+	i, j := 0, 0 //这算是双指针办法吧
 	for i < gLen && j < sLen {
-		if  g[i]<= s[j] {
+		if g[i] <= s[j] { //如果当前饼干大于当前小孩胃口，那么满足，饼干和小孩胃口同时移动指针
 			result += 1
 			i += 1
 			j += 1
 
-		} else {
+		} else { //如果饼干尺寸不满足小孩胃口，那么饼干指针右移动指向下一个更大的
 			j += 1
 
 		}
 	}
+	//每一步都只满足放下这一步满足即可
 	return result
 
 }
