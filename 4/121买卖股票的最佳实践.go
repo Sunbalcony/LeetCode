06cf2c5e9ee971eb57aa785512a83c45cfd9b0c1
Sunbalcony <160632460@qq.com>
2021-09-05 22:59:45 +0800

    Create 121买卖股票的最佳实践.go

---
+++ 4/121买卖股票的最佳实践.go
@@ -0,0 +1,14 @@
+package main
+
+//https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+
+//贪心问题
+//买卖股票只要保证每天能挣得差价，那么基本上整体就会得到最丰厚的收益
+//我们现在是上帝视角，可以看到每天的股票价格，按照贪算法的要求，保证每天利益最大化即可
+func maxProfit(prices []int) int {
+
+}
+
+func main() {
+
+}
